57ae29d67cadbe2547a012dd4fce07ec514607fc
shivakrishna67 <125286143+shivakrishna67@users.noreply.github.com>
2023-11-28 17:03:38 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='Motor vehicle collisions',
+    name='Motor_vehicle_collisions',
     version='0.2.1',
     description='Classes for technical analysis of building dataset.',
     author='Shivakrishna Macha',
     author_email='smacha@mail.yu.edu',
     license='MIT',
     url='https://github.com/shivakrishna67/AnalyticalProgramming-Project-3',
-    packages=['Motor_vehicle_crashes'],
+    packages=['Motor_vehicle_collisions'],
     install_requires=[
         'matplotlib>=3.7.1',
         'numpy>=1.24.3',
